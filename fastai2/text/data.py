193c13d1ee3679be3d84fff0f3f9d0f1ca2641ab
Morgan <morganmcg1@users.noreply.github.com>
2020-07-09 12:44:14 +0100

    Adds additional documentation for SortedDLand modifies SortedDL.new to accept val_res

--- fastai2/text/data.py
+++ fastai2/text/data.py
@@ -63,6 +63,7 @@ def _get_lengths(ds):
     return tok.get_lengths(ds.items)
 
 # Cell
+#TODO: add backward
 @log_args(but_as=TfmdDL.__init__)
 @delegates()
 class LMDataLoader(TfmdDL):
@@ -179,7 +180,8 @@ class SortedDL(TfmdDL):
 
     @delegates(TfmdDL.new)
     def new(self, dataset=None, **kwargs):
-        res = self.res if dataset is None else None
+        if kwargs['val_res'] is not None: res = kwargs['val_res']
+        else: res = self.res if dataset is None else None
         return super().new(dataset=dataset, res=res, **kwargs)
 
 # Cell
